869670ea700f6ea3a322a9c34a462de8fed35ba9
subha20032007 <meunknown466@gmail.com>
2024-01-09 00:48:16 +0530

    Ad

--- Week2/Day2/index.js
+++ Week2/Day2/index.js
@@ -5,19 +5,8 @@ const app=express()
 app.get("/",(req,res)=>{
     res.send("<h1>Home (:.:) Page</h1>")
 })
-app.get("/students",(req,res)=>{
-    res.send("<h1>Student Page</h1>")
-})
 
-app.post("/addStudents",(req,res)=>{
-    res.send("<h1>Add students</h1>")
-})
-app.get("/teachers",(req,res)=>{
-    res.send("<h1>Teacher Page</h1>")
-})
-app.post("addTeachers",(req,res)=>{
-    res.send("<h1>Add Teacher</h1>")
-})
+
 app.listen(9090,()=>{
     console.log("Port run in 9090")
 })
