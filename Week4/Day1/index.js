f8f3b1b81cc8499425be087e17a44b3ea1b961b1
subha20032007 <meunknown466@gmail.com>
2024-01-19 02:16:00 +0530

    Ad|

--- Week4/Day1/index.js
+++ Week4/Day1/index.js
@@ -1,1 +1,12 @@
-//12345678
+const express=require("express")
+const Port=8080
+const app=express()
+
+
+app.get("/",(req,res)=>{
+    res.send("<h1>Home Page</h1>")
+})
+
+app.listen(Port,()=>{
+    console.log(`App running on port ${Port}`)
+})
